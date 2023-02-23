4de4f413261b3c2278fc4d553da5735adc5b5cd1
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-23 08:44:37 +0100

    Enlace a noticia nacional1 desde portada funcionando

--- src/Vistas/Principal/Principal.jsx
+++ src/Vistas/Principal/Principal.jsx
@@ -5,10 +5,11 @@ import Articulos from "../Articulos/Articulos";
 
 function Principal() {
 
-    /*const[Nacional1, SetNacional1] = useState(false)
+    const[Nacional1, setNacional1] = useState(false)
     function manexadorNacional1(){
-    SetNacional1(true)
-    }*/
+      let nuevoValor=true
+      setNacional1(nuevoValor)
+    }
 
     return (
       <main>
@@ -17,8 +18,8 @@ function Principal() {
         <section className={styles.secciones}>
         <h2 className={styles.Seccion}>SECCION NACIONAL</h2>
         <div className={styles.ArtSections}>
-        <article className={styles.articles}>
-        <Noticias Titulo="Tit.Portada Noticia Nacional1" /*onClick={manexadorNacional1}*/ Contenido="Contenido.Portada Noticia Nacional 1, Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1"/>
+        <article className={styles.articles} onClick={manexadorNacional1}>
+        <Noticias Titulo="Tit.Portada Noticia Nacional1" Contenido="Contenido.Portada Noticia Nacional 1, Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1Contenido.Portada Noticia Nacional 1"/>
         </article>
         <article className={styles.articles}>
         <Noticias Titulo="Tit.Portada Noticia Nacional 2" Contenido="Contenido.Portada Noticia Nacional 2"/>
@@ -38,7 +39,7 @@ function Principal() {
         </div>
         </section>
 
-        {/*Nacional1 && <Articulos/>*/}
+        {Nacional1===true && <Articulos/>}
 
       </main>
     );
